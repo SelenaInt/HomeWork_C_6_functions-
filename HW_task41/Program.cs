f75e81ea6e7ee113c207537c0c65b1ab826083d4
SelenaInt <selen15@yandex.ru>
2022-07-12 01:11:52 +0500

    HW_task41: how many numbers are greater than 0

---
+++ HW_task41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите колличество чисел: ");
+int sizeNumbers = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < sizeNumbers; i++)
+{
+    Console.Write($"Введите положительное или отрицательное число {i + 1}: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0)
+        count++;
+}
+Console.WriteLine($"Количество Положительных чисел: {count}");
